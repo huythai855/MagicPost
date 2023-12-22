39143c5b761129af7209580e630dce9ba46f61c7
응우옌 후이 타이 <nguyenhuythai15@gmail.com>
2023-12-22 22:26:55 +0000

    feat: add employee controller for director

--- server/index.js
+++ server/index.js
@@ -2,15 +2,18 @@ const bodyParser = require('body-parser');
 const express = require('express')
 const path = require('path')
 const sqlite3 = require('sqlite3').verbose();
+const cookieParser = require('cookie-parser');
+
 
 const db = new sqlite3.Database('open.db');
 const app = express()
-const port = 3000
+const port = 3001
 
+app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname + '/public')));
 app.use(bodyParser.json());
 
-
+/// TEST
 app.get('/login', (req, res, next) => {
     res.sendFile(__dirname + '/public/login.html')
 })
@@ -18,8 +21,13 @@ app.get('/login', (req, res, next) => {
 app.get('/home', (req, res, next) => {
     res.sendFile(__dirname + '/public/home.html')
 })
+///
+
+
 
 app.post('/api/login', (req, res, next) => {
+    next();
+}, (req, res, next) => {
     console.log(req.body);
 
     var username = req.body.username;
@@ -28,7 +36,7 @@ app.post('/api/login', (req, res, next) => {
     console.log(username);
     console.log(password);
 
-    db.all("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], (err, rows) => {
+    db.all("SELECT * FROM employees WHERE username = ? AND password = ?", [username, password], (err, rows) => {
         if (err) {
             console.log(err);
             res.json({ message: 'Login failed' })
@@ -36,24 +44,60 @@ app.post('/api/login', (req, res, next) => {
         if (rows.length == 0) {
             res.json({ message: 'Login failed' })
         } else {
+            console.log(rows);
+            var role = rows[0].role;
+            var id = rows[0].id;
+            var fullname = rows[0].fullname;
+
+            // res.json({
+            //     message: 'Login success',
+            //     username: username,
+            //     role: role,
+            //     id: id,
+            //     fullname: fullname,
+            // });
+
+
             res.json({
                 message: 'Login success',
-                user: username
+                username: username
             });
             
         }
     });
 
-})
+});
 
 
-
-
-app.get('/', (req, res, next) => {
-    res.send('Hello World!')
-
-
+app.get('/employees', (req, res, next) => {
+    var role = req.cookies.role;
+    console.log(role);
+    if(role == undefined) 
+        res.json({ message: 'You are not allowed to access this page' });
+    if(role == 'shipper' || role == 'transaction_point_staff' || role =='gathering_point_staff') {
+        res.json({ message: 'You are not allowed to access this page' });
+    }
+    next();
 }, (req, res, next) => {
+    var role = req.cookies.role;
+
+    if(role == 'director') {
+        db.all("SELECT * FROM employees", (err, rows) => {
+            if (err) {
+                console.log(err);
+                res.json({ message: 'Login failed' })
+            } else {
+                console.log(rows);
+            }
+        });
+    }
+    
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send('Homepage. Nothing to see here!')
 })
 
 
